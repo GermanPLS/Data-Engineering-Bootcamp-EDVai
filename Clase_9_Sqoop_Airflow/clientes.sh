1b63628425e9a37e3f983a9a6763c4a9f130e4c1
German Leventan <165523910+GermanPLS@users.noreply.github.com>
2024-06-05 20:09:45 -0300

    Add files via upload

--- Clase_9_Sqoop_Airflow/clientes.sh
+++ Clase_9_Sqoop_Airflow/clientes.sh
@@ -4,7 +4,7 @@
 sqoop import \
 --connect jdbc:postgresql://172.17.0.3:5432/northwind \
 --username postgres \
---password-file file:///home/hadoop/scripts/password-file.txt \
+--password-file file:///home/hadoop/scripts/sqoop_edvai_postgres.password \
 --query "SELECT cu.customer_id, cu.company_name, SUM(od.quantity) AS productos_vendidos FROM orders AS o INNER JOIN order_details AS od ON o.order_id = od.order_id LEFT JOIN customers AS cu ON o.customer_id = cu.customer_id WHERE $CONDITIONS GROUP BY cu.customer_id, cu.company_name ORDER BY productos_vendidos DESC" \
 --m 1 \
 --target-dir /sqoop/ingest/clientes \
