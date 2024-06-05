8e84d8f4a50e883f06a34a84af4d33c72873a4fd
German Leventan <165523910+GermanPLS@users.noreply.github.com>
2024-06-05 20:12:53 -0300

    Add files via upload

--- Clase_9_Sqoop_Airflow/detalles.sh
+++ Clase_9_Sqoop_Airflow/detalles.sh
@@ -13,15 +13,3 @@ sqoop import \
 --delete-target-dir
 
 
-
-/home/hadoop/hadoop/bin/hdfs dfs -rm -r /sqoop/ingest/order_details/*
-
-sqoop import \
---connect jdbc:postgresql://172.17.0.3:5432/northwind \
---username postgres \
---password-file file:///home/hadoop/sqoop/scripts/sqoop.pass \
---query "select * from region where \$CONDITIONS limit 10 " \
---m 1 \
---target-dir /sqoop/ingest/order_details \
---as-parquetfile \
---delete-target-dir
